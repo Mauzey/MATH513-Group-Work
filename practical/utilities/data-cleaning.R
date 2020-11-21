87665377b15d9e8de1fd8b60d855ec91450f77f5
Alex Mounsey <atmounsey@gmail.com>
2020-11-21 04:38:12 +0000

    Updated files (See Desc.)
    - Refactored data imports to make use of the new file structure. This will save us a headache in the future
    
    - Refactored section headers so they appear in the RStudio dropdown navigation
    
    - Moved data cleaning functionality from '02 analyse_tweets.r' to 'data-cleaning.R'. Didn't realise that this file contained data cleaning when creating 'data-cleaning.R' - it's now all in one place.

--- practical/utilities/data-cleaning.R
+++ practical/utilities/data-cleaning.R
@@ -2,6 +2,9 @@
 #   Import Tweet/User Data and Clean it
 # 
 
+# CHANGE LOG ----------------------------------------------------------------------------------------------------------------
+## 2020-11-21 - AM  - Created file and set up structure with basic data cleaning
+
 # IMPORT DEPENDENCIES / SETUP -----------------------------------------------------------------------------------------------
 
 library(dplyr)
@@ -9,31 +12,65 @@ library(readr)
 
 #setwd() # Will
 #setwd() # Tania
-#setwd("~/Development/Repositories/MATH513-Group-Work/practical")  # Alex laptop
-#setwd("D:/Development/MATH513-Group-Work/practical")  # Alex desktop
+#setwd("~/Development/Repositories/MATH513-Group-Work/practical")  # Alex
+#setwd("D:/Development/MATH513-Group-Work/practical")              #
 
-# IMPORT DATA ---------------------------------------------------------------------------------------------------------------
+# IMPORT / COMBINE DATA -----------------------------------------------------------------------------------------------------
 
-tweets_dir <- "./data/tweets"  # Directory of tweet data
-users_dir <- "./data/users"  # Directory of user data
-
-# Import tweet data
-for (file in list.file(path = tweets_dir, pattern = '*.csv', full.names = T)) {
-  # If 'tweets' dataframe doesn't exist:
-    # Import file into 'tweets' dataframe
-  
-  # Elif 'tweets' dataframe does exist:
-    # Append file to 'tweets' dataframe
+for (file in list.file(path = "./data/tweets", pattern = '*.csv', full.names = T)) {
+  # if (tweets dataframe doesn't exist):
+  #   tweets <- file
+  # else:
+  #   append file to tweets
 }
 
-# Import user data
-for (file in list.file(path = users_dir, pattern = '*.csv', full.names = T)) {
-  # If 'users' dataframe doesn't exist:
-    # Import file into 'users' dataframe
-  
-  # Elif 'users' dataframe does exist:
-    # Append file to 'users' dataframe
+for (file in list.file(path = "./data/users", pattern = '*.csv', full.names = T)) {
+  # if (users dataframe doesn't exist):
+  #   users <- file
+  # else:
+  #   append file to users
 }
+
+# PRE-CLEANING ANALYSIS -----------------------------------------------------------------------------------------------------
+
+names(tweets)  # Column names
+names(users)   #
+
+dim(tweets)    # Dataset shape
+dim(users)     #
+
+# * Time-Series Plots to Check Dates ----------------------------------------------------------------------------------------
+
+# Suggestion: add a column during data cleaning which specifies which product a tweet is referring to, then plot all three 
+# ts_plots on a single graph with different colours (using 'filter()'). We'll also be able to use this column for facet_grids
+# in the future
+
+ts_plot(iphone12_tweets, "secs", trim = 1) + 
+  theme_minimal() + 
+  theme(plot.title = element_text(face = "bold")) +
+  labs(x = NULL, y = NULL,
+       title = "Frequency of Twitter statuses for #iPhone12",
+       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
+       caption = "Source: Data collected from Twitter's REST API via rtweet"
+  )
+
+ts_plot(s20fe_tweets, "secs", trim = 1) + 
+  theme_minimal() + 
+  theme(plot.title = element_text(face = "bold")) +
+  labs(x = NULL, y = NULL,
+       title = "Frequency of Twitter statuses for #GalaxyS20FE",
+       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
+       caption = "Source: Data collected from Twitter's REST API via rtweet"
+  )
+
+ts_plot(s20_tweets, "secs", trim = 1) + 
+  theme_minimal() + 
+  theme(plot.title = element_text(face = "bold")) +
+  labs(x = NULL, y = NULL,
+       title = "Frequency of Twitter statuses for #GalaxyS20",
+       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
+       caption = "Source: Data collected from Twitter's REST API via rtweet"
+  )
 
 # DATA CLEANING -------------------------------------------------------------------------------------------------------------
 
@@ -54,6 +91,6 @@ users$location[users$location == ''] <- NA
 # EXPORT CLEANED DATA -------------------------------------------------------------------------------------------------------
 
 for (dir in c('./data/', './data/backup/')) {
-  write.csv(tweets, paste0(dir, 'combined-tweets.csv'))
-  write.csv(users, paste0(dir, 'combined-users.csv'))
+  write.csv(tweets, paste0(dir, 'cleaned-tweets.csv'))
+  write.csv(users, paste0(dir, 'cleaned-users.csv'))
 }
