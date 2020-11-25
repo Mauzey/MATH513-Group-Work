f9553ffb7267d548c0d7957cf8be0a028de304ad
Alex Mounsey <atmounsey@gmail.com>
2020-11-25 02:38:18 +0000

    Refactored codebase

--- practical/utilities/02-data-cleaning.R
+++ practical/utilities/02-data-cleaning.R
@@ -2,91 +2,102 @@
 #   Import Tweet/User Data and Clean it
 # 
 
-# CHANGE LOG ----------------------------------------------------------------------------------------------------------------
-## 2020-11-21 - AM  - Created file and set up structure with basic data cleaning
+# IMPORT DEPENDENCIES -------------------------------------------------------------------------------------------------------
 
-# IMPORT DEPENDENCIES / SETUP -----------------------------------------------------------------------------------------------
-
-library(dplyr)
 library(readr)
+library(dplyr)
 
-#setwd("C:/Users/willo/SynologyDrive/Documents - Will/Qualifications/!MSc Data Science/!Courses/1 MATH513 Big Data and Social Network/Assessment/MATH513-Group-Work/practical") # Will
-#setwd() # Tania
-#setwd("~/Development/Repositories/MATH513-Group-Work/practical")  # Alex
-#setwd("D:/Development/MATH513-Group-Work/practical")              #
+# SET WORKING DIRECTORY -----------------------------------------------------------------------------------------------------
 
-# IMPORT / COMBINE DATA -----------------------------------------------------------------------------------------------------
+# Will
+#setwd("C:/Users/willo/SynologyDrive/Documents - Will/Qualifications/!MSc Data Science/!Courses/1 MATH513 Big Data and Social Network/Assessment/MATH513-Group-Work/practical")
 
-for (file in list.file(path = "./data/tweets", pattern = '*.csv', full.names = T)) {
-  # if (tweets dataframe doesn't exist):
-  #   tweets <- file
-  # else:
-  #   append file to tweets
+# Tania
+#setwd("")
+
+# Alex
+#setwd("~/Development/Repositories/MATH513-Group-Work/practical")
+#setwd("D:/Development/MATH513-Group-Work/practical")
+
+# IMPORT & COMBINE TWEET DATA -----------------------------------------------------------------------------------------------
+
+# Define columns to keep from the raw tweet data
+# The raw tweet data is not going to be overwritten - if you think more columns could be useful, add them to this vector
+tweet_cols_to_keep = c('user_id', 'status_id', 'created_at', 'text', 'source', 'is_retweet', 'favorite_count', 'retweet_count',
+                       'quote_count', 'reply_count', 'hashtags', 'media_type', 'lang')
+
+# Iterate over each file in the 'tweets' directory
+for (file in list.files(path = "./data/tweets", pattern = '*.csv', full.names = T)) {
+  new_data <- read_csv(file)[tweet_cols_to_keep]  # read in the file
+  
+  # Determine which product the file is referring to, and add a column to mark the data as such
+  if (grepl('iphone12-', file)) {
+    new_data$product <- 'iPhone12'
+  } else if (grepl('s20-', file)) {
+    new_data$product <- 'Galaxy S20'
+  } else if (grepl('s20fe-', file)) {
+    new_data$product <- 'Galaxy S20 FE'
+  }
+  
+  # If a dataframe containing all tweets doesn't exist, create it with the first file that's read in
+  if (!exists('complete_tweet_data')) {
+    complete_tweet_data <- new_data
+  # Otherwise, join the file to the existing tweets
+  } else {
+    complete_tweet_data <- rbind(complete_tweet_data, new_data)
+  }
+  
+  rm(new_data)  # delete the 'new_data' variable from memory to keep the environment tidy
 }
 
-for (file in list.file(path = "./data/users", pattern = '*.csv', full.names = T)) {
-  # if (users dataframe doesn't exist):
-  #   users <- file
-  # else:
-  #   append file to users
+
+# IMPORT & COMBINE USER DATA ------------------------------------------------------------------------------------------------
+
+# Define columns to keep from the raw user data
+# The raw user data is not going to be overwritten - if you think more columns could be useful, add them to this vector
+user_cols_to_keep = c('user_id', 'screen_name', 'name', 'location', 'description', 'followers_count', 'friends_count',
+                      'listed_count', 'statuses_count', 'favourites_count', 'account_created_at', 'verified')
+
+# Iterate over each file in the 'users' directory
+for (file in list.files(path = "./data/users", pattern = '*.csv', full.names = T)) {
+  new_data <- read_csv(file)[user_cols_to_keep]  # read in the file
+  
+  # If a dataframe containing all users doesn't exist, create it with the first file that's read in
+  if (!exists('complete_user_data')) {
+    complete_user_data <- new_data
+  # Otherwise, join the file to the existing tweets
+  } else {
+    complete_user_data <- rbind(complete_user_data, new_data)
+  }
+  
+  rm(new_data)  # delete the 'new_data' variable from memory to keep the environment tidy
 }
-
-# PRE-CLEANING ANALYSIS -----------------------------------------------------------------------------------------------------
-
-names(tweets)  # Column names
-names(users)   #
-
-dim(tweets)    # Dataset shape
-dim(users)     #
-
-# TIME SERIES PLOTS TO CHECK DATES ----------------------------------------------------------------------------------------
-
-# Suggestion: add a column during data cleaning which specifies which product a tweet is referring to, then plot all three 
-# ts_plots on a single graph with different colours (using 'filter()'). We'll also be able to use this column for facet_grids
-# in the future
-
-ts_plot(iphone12_tweets, "secs", trim = 1) + 
-  theme_minimal() + 
-  theme(plot.title = element_text(face = "bold")) +
-  labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for #iPhone12",
-       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
-       caption = "Source: Data collected from Twitter's REST API via rtweet"
-  )
-
-ts_plot(s20fe_tweets, "secs", trim = 1) + 
-  theme_minimal() + 
-  theme(plot.title = element_text(face = "bold")) +
-  labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for #GalaxyS20FE",
-       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
-       caption = "Source: Data collected from Twitter's REST API via rtweet"
-  )
-
-ts_plot(s20_tweets, "secs", trim = 1) + 
-  theme_minimal() + 
-  theme(plot.title = element_text(face = "bold")) +
-  labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for #GalaxyS20",
-       subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
-       caption = "Source: Data collected from Twitter's REST API via rtweet"
-  )
 
 # DATA CLEANING -------------------------------------------------------------------------------------------------------------
 
-# Remove unnecessary columns from the tweets data
-cols_to_keep <- c('', '', '')
-tweets <- tweets[cols_to_keep]
+# Remove duplicate entries
+complete_tweet_data <- distinct(complete_tweet_data, status_id, .keep_all = T)
+complete_user_data <- distinct(complete_user_data, user_id, .keep_all = T)
 
-# Remove unnecessary columns from the user data
-cols_to_keep <- c('', '', '')
-users <- users[cols_to_keep]
+# Convert blank location values to 'NA'
+# ...
 
-# Remove duplicate rows from the user data; we only need one entry per user
-users <- distinct(users, user_id, .keep_all = T)
+# EXPORT CLEANED DATA -------------------------------------------------------------------------------------------------------
 
-# Convert blank location entries into 'NA'
-users$location[users$location == ''] <- NA
+for (dir in c("./data/", "./data/backup/")) {
+  write.csv(complete_tweet_data, paste0(dir, "cleaned-tweets.csv"))
+  write.csv(complete_user_data, paste0(dir, "cleaned-users.csv"))
+}
+
+# DATA SUMMARY --------------------------------------------------------------------------------------------------------------
+
+# List column names
+names(tweets)
+names(users)
+
+# Show dataframe shape
+dim(tweets)
+dim(users)
 
 # EXPORT CLEANED DATA -------------------------------------------------------------------------------------------------------
 
--- practical/utilities/03-analyse_tweets.R
+++ practical/utilities/03-analyse_tweets.R
@@ -2,61 +2,49 @@
 #   Analysis of Twitter Data
 # 
 
-# CHANGE LOG ----------------------------------------------------------------------------------------------------------------
-## 2020-11-20 - WON - Setup of initial file
-##                  - Added dependencies, variables section, .csv read, combination of data sets and export, quick analysis,
-##                    clean data section, quick analysis post lang strip
+# IMPORT DEPENDENCIES -------------------------------------------------------------------------------------------------------
 
-## 2020-11-21 - AM  - Refactored section headers so they appear in the navigation dropdown (margin column = 130)
-##                  - Moved 'Combining Data' section to its own file: './data-cleaning.R'
-##                  - Moved 'Quick Analysis' section to its own file: './data-cleaning.R'
+library(readr)
+library(ggplot2)
+library(ggthemes)
+library(tidyverse)
+library(dplyr)
+library(modelr)
 
-# IMPORT DEPENDENCIES / SETUP -----------------------------------------------------------------------------------------------
+# SET WORKING DIRECTORY -----------------------------------------------------------------------------------------------------
 
-library(readr)      # Read in data from .csv
-library(ggplot2)    # Graphing data
-library(ggthemes)   # Graphing themes
-library(tidyverse)  # Manipulating data
-library(dplyr)      # Manipulating data
-library(alr4)       # Statistical analysis of data
-library(modelr)     # Modeling data
+# Will
+#setwd("C:/Users/willo/SynologyDrive/Documents - Will/Qualifications/!MSc Data Science/!Courses/1 MATH513 Big Data and Social Network/Assessment/MATH513-Group-Work/practical")
 
-#setwd() # Will
-#setwd() # Tania
-#setwd("~/Development/Repositories/MATH513-Group-Work/practical")  # Alex
-#setwd("D:/Development/MATH513-Group-Work/practical")  # Alex
+# Tania
+#setwd("")
 
-# DEFINE LOCAL VARIABLES ----------------------------------------------------------------------------------------------------
-
-# ...
+# Alex
+#setwd("~/Development/Repositories/MATH513-Group-Work/practical")
+#setwd("D:/Development/MATH513-Group-Work/practical")
 
 # IMPORT DATA ---------------------------------------------------------------------------------------------------------------
 
 tweets <- read_csv("./data/cleaned-tweets.csv")
 users <- read_csv("./data/cleaned-users.csv")
 
-#----------------------------------------------------------------------------------------
-## QUICK ANALYSIS - BEFORE DATA CLEAN UP
-#----------------------------------------------------------------------------------------
+# DATA SUMMARY --------------------------------------------------------------------------------------------------------------
 
-names(iphone12_tweets)  # Column names
-names(s20fe_tweets)     # Column names
-names(s20_tweets)       # Column names
+# Print the columns of the dataframes
+names(tweets)
+names(users)
 
-dim(iphone12_tweets)    # Dimensions of the data set
-dim(s20fe_tweets)       # Dimensions of the data set
-dim(s20_tweets)         # Dimensions of the data set
+# Print the shape of the data (rows/columns)
+dim(tweets)
+dim(users)
 
-no_tweets_iphone12 <- nrow(iphone12_tweets)  # Number of rows of data
-no_tweets_s20fe <-nrow(s20fe_tweets)         # Number of rows of data
-no_tweets_s20_ <-nrow(s20_tweets)            # Number of rows of data
+# TIME SERIES PLOTS ---------------------------------------------------------------------------------------------------------
 
-### Time series plot to check dates
 ts_plot(iphone12_tweets, "secs", trim = 1) + 
   theme_minimal() + 
   theme(plot.title = element_text(face = "bold")) +
   labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for " + iphone12_query,
+       title = "Frequency of Twitter statuses for #iPhone12",
        subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
        caption = "Source: Data collected from Twitter's REST API via rtweet"
   )
@@ -65,7 +53,7 @@ ts_plot(s20fe_tweets, "secs", trim = 1) +
   theme_minimal() + 
   theme(plot.title = element_text(face = "bold")) +
   labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for " + s20fe_query,
+       title = "Frequency of Twitter statuses for #GalaxyS20FE",
        subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
        caption = "Source: Data collected from Twitter's REST API via rtweet"
   )
@@ -74,26 +62,7 @@ ts_plot(s20_tweets, "secs", trim = 1) +
   theme_minimal() + 
   theme(plot.title = element_text(face = "bold")) +
   labs(x = NULL, y = NULL,
-       title = "Frequency of Twitter statuses for " + s20_query,
+       title = "Frequency of Twitter statuses for #GalaxyS20",
        subtitle = "Twitter status (tweet) counts aggregated using 1-seconds intervals",
        caption = "Source: Data collected from Twitter's REST API via rtweet"
   )
-
-#----------------------------------------------------------------------------------------
-## CLEAN DATA BASED ON LANGUAGE
-#----------------------------------------------------------------------------------------
-
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  # Do we need to actually remove tweets for some of the data analysis and visualisations?
-  # We will need to for sentiment analysis as this works on the English language but other stats should work fine like time series etc?
-
-
-
-
-#----------------------------------------------------------------------------------------
-## QUICK ANALYSIS - POST LANGUAGE STRIP FOR ENGLISH ONLY
-#----------------------------------------------------------------------------------------
-
-no_tweets_iphone12_en <- nrow(iphone12_tweets)       # Number of rows of data
-no_tweets_s20fe_en <-nrow(s20fe_tweets)              # Number of rows of data
-no_tweets_s20_en <-nrow(s20_tweets)                  # Number of rows of data
